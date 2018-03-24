a4402079af6efa464336b507db11ea72372f8271
beingames4u <31393098+beingames4u@users.noreply.github.com>
2018-03-24 17:28:04 +0300

    Update datee.js

--- datee.js
+++ datee.js
@@ -11,6 +11,6 @@ if (daym<10)
 daym="0"+daym
 var dayarray=new Array("الأحد","الإثنين","الثلاثاء","الأربعاء","الخميس","الجمعة","السبت")
 var montharray=new Array("يناير","فبراير","مارس","ابريل","مايو","يونيو","يوليو","أغسطس","سبتمبر","أكتوبر","نوفمبر","ديسمبر")
-document.write("<span class='cnmdaylabel'>"+dayarray[day]+"<\/span><span class='fasel'> / <\/span><span class='daylnum'>"+daym+"<\/span><span class='fasel'> / <\/span><span class='month'>"+montharray[month]+"<\/span><span class='fasel'> / <\/span><span class='year'>"+year+"<\/span><span class='melady'> م <\/span>")
+document.write("<span class='cnmdaylabel'>"+dayarray[day]+"<\/span><span class='fasel'> / <\/span><span class='daylnum'>"+daym+"<\/span><span class='fasel'> <\/span><span class='month'>"+montharray[month]+"<\/span><span class='fasel'> / <\/span><span class='year'>"+year+"<\/span><span class='melady'> م <\/span>")
 document.write("<\/div>");
 /*]]>*/
